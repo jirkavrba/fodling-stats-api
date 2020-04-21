4b3c9bec0d13926244d9319194298cb8e76fccc1
Jiří Vrba <jiri@vrba.dev>
2020-04-21 13:55:52 +0200

    Team -> Institution database relation

--- app/Team.php
+++ app/Team.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'folding_id',
+        'name',
+        'institution_id',
+    ];
+
+    public function institution(): BelongsTo
+    {
+        return $this->belongsTo(Institution::class, 'institution_id', 'id');
+    }
 }
