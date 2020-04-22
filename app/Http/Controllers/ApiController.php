2bcb92fc65572e90bf792cf2728028ac09993175
Jiří Vrba <jiri@vrba.dev>
2020-04-22 18:53:25 +0200

    Teams API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,23 +6,39 @@ use App\Institution;
 use App\Team;
 use DateTime;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
 
 class ApiController extends Controller
 {
     public function institutions(): JsonResponse
     {
-        $data = [
+        return response()->json([
             'institutions' => Institution::apiRepresentation()
-        ];
+        ]);
+    }
 
-        return response()->json($data);
+    public function teams(): JsonResponse
+    {
+        $teams = Arr::flatten(
+            // Map all institutions' teams to respect their logo/color
+            Institution::apiRepresentation()->map(
+                function ($institution) {
+                    return $institution->teams->each(function ($team) use ($institution) {
+                        $team['logo'] = $institution->logo;
+                        $team['color'] = $institution->color;
+                    });
+                }
+            )
+        );
+
+        return response()->json(['teams' => $teams]);
     }
 
     public function team(Team $team): JsonResponse
     {
         // Convert saved datetime to unix timestamp
         $data = $team->results
-                     ->each(fn ($result) => $result->datetime = (new DateTime($result->datetime))->getTimestamp());
+            ->each(fn($result) => $result->datetime = (new DateTime($result->datetime))->getTimestamp());
 
         return response()->json($data);
     }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/institutions", [ApiController::class, 'institutions']);
+Route::get("/teams", [ApiController::class, 'teams']);
 Route::get("/team/{team}", [ApiController::class, 'team']);
